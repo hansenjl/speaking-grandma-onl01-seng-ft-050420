e2891fbac0a60bf07d0ba7ab311d55f16488d06c
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-05 15:21:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,13 +16,12 @@
 def speak_to_grandma(phrase)
   # if the phrase is the same as the phrase when all caps
   if phrase == 'I LOVE YOU GRANDMA!'
-    puts  'I LOVE YOU TOO PUMPKIN!'
-    
+    'I LOVE YOU TOO PUMPKIN!'
   elsif  phrase == phrase.upcase 
-    puts 'NO, NOT SINCE 1938!'
-   else 
-    puts  'HUH?! SPEAK UP, SONNY!'
-    end
+    'NO, NOT SINCE 1938!'
+  else 
+    'HUH?! SPEAK UP, SONNY!'
+  end
 
 end
 
