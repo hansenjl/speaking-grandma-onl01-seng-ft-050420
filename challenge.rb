6a3282f8bc0207f691b3dc296434e0fb6261b33a
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-05 15:40:05 +0000

    Automatically backed up by Learn

--- challenge.rb
+++ challenge.rb
@@ -1,8 +1,8 @@
 # CHALLENGE 1: write an if statement and then a ternary statement to determine how much you should pay for your cab.  If it is a trip under 5 miles, you own $10.  If it is a trip over 5 miles, you will owe $1 per mile + a base far of $5. For example, if you take a 10 mile trip, it will cost (10 * $1 + $5) = $15.
 
 
-def cab_cost
-  
+def cab_cost()
+end
 
 
 
